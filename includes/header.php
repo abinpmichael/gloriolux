ad1298d0ea4c9e8d208f654c421f1571f86eee2b
Abin P Michael <abinp.michael@gmail.com>
2026-04-28 16:09:21 -0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -20,9 +20,9 @@ require_once __DIR__.'/db.php';
         $page_meta_keywords = $seo_data ? $seo_data['meta_keywords'] : 'candles, luxury, soy wax';
     }
     ?>
-    <title><?php echo htmlspecialchars($page_meta_title); ?></title>
-    <meta name="description" content="<?php echo htmlspecialchars($page_meta_desc); ?>">
-    <meta name="keywords" content="<?php echo htmlspecialchars($page_meta_keywords); ?>">
+    <title><?php echo htmlspecialchars($page_meta_title ?? 'Gloriolux'); ?></title>
+    <meta name="description" content="<?php echo htmlspecialchars($page_meta_desc ?? 'Premium hand-poured luxury soy candles.'); ?>">
+    <meta name="keywords" content="<?php echo htmlspecialchars($page_meta_keywords ?? 'candles, luxury, soy wax'); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
